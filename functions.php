9419a2719801c129af51e7424e349092673af871
Raffol <eremenko97@bk.ru>
2025-11-17 16:19:26 +0800

    feat: добавление шаблонизации

---
+++ functions.php
@@ -0,0 +1,60 @@
+<?php
+// Подключение стилей и скриптов
+add_action( 'wp_enqueue_scripts', 'testbitrix_scripts' );
+function testbitrix_scripts() {
+
+    // Основной style.css темы
+    wp_enqueue_style(
+        'theme-style',
+        get_stylesheet_uri()
+    );
+
+    // Bootstrap
+    wp_enqueue_style(
+        'bootstrap',
+        get_template_directory_uri() . '/assets/vendor/bootstrap/css/bootstrap.min.css',
+        [],
+        null
+    );
+
+    // Bootstrap Icons
+    wp_enqueue_style(
+        'bootstrap-icons',
+        get_template_directory_uri() . '/assets/vendor/bootstrap-icons/bootstrap-icons.css',
+        [],
+        null
+    );
+
+    // AOS animations
+    wp_enqueue_style(
+        'aos',get_template_directory_uri() . '/assets/vendor/aos/aos.css',[],null
+    );
+
+    // Основной main.css
+    wp_enqueue_style(
+        'main-style',get_template_directory_uri() . '/assets/css/main.css',
+        ['bootstrap'], // можно указать зависимости
+        null
+    );
+
+    //Если нужен JS пример:
+    wp_enqueue_script(
+        'bootstrap.bundle.min',
+        get_template_directory_uri() . '/assets/vendor/bootstrap/js/bootstrap.bundle.min.js',
+        [],
+        '',
+        true
+    );wp_enqueue_script(
+        'aos',
+        get_template_directory_uri() . '/assets/vendor/aos/aos.js',
+        [],
+        '',
+        true
+    );wp_enqueue_script(
+        'main',
+        get_template_directory_uri() . '/assets/js/main.js',
+        [],
+        '',
+        true
+    );
+}
---
+++ header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php wp_head(); ?>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>Компания - шаблон главной страницы</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+</head>
+
+<body class="scrolled">
+
+<header id="header" class="header d-flex align-items-center">
+    <div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+
+        <a href="#" class="logo d-flex align-items-center">
+            <h1 class="sitename">Компания</h1>
+        </a>
+
+        <nav id="navmenu" class="navmenu">
+            <ul>
+                <li><a href="#">Главная</a></li>
+                <li><a href="#">Пункт 1</a></li>
+                <li><a href="#">Пункт 1</a></li>
+                <li><a href="#">Пункт 3</a></li>
+                <li><a href="#">Пункт 4</a></li>
+                <li class="dropdown">
+                    <a href="#"><span>Пункт 5 с подменю</span>
+                        <i class="bi bi-chevron-down toggle-dropdown"></i>
+                    </a>
+                    <ul>
+                        <li><a href="#">Пункт 1</a></li>
+                        <li class="dropdown"><a href="#"><span>Пункт 2 с подменю</span> <i
+                                    class="bi bi-chevron-down toggle-dropdown"></i></a>
+                            <ul>
+                                <li><a href="#">Пункт 1</a></li>
+                                <li><a href="#">Пункт 2</a></li>
+                                <li><a href="#">Пункт 3</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Пункт 2</a></li>
+                        <li><a href="#">Пункт 3</a></li>
+                        <li><a href="#">Пункт 4</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Пункт 6</a></li>
+            </ul>
+            <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+        </nav>
+
+    </div>
+</header>
